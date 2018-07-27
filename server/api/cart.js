500cce8d62278033ce48b633af8cab7dbc78abbf
Abigail Demsas <ademsas1026@gmail.com>
2018-07-27 14:26:32 -0400

    some changes

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,37 +4,34 @@ const { Order, Product, LineItem } = require('../db/models');
 module.exports = router;
 
 router.use( async (req, res, next) => {
-  if (req.cart || !req.user) return next();
-
-  if (req.session.cartId) {
-    req.cart = await Order.findById(req.session.cartId).catch(next);
-    if (req.cart && req.user) req.cart.userId = req.user.id;
-    if (req.cart) return next();
+  //logged in user
+  if (req.user){
+    const [order, _] = await Order.findOrCreate({ where: { userId: req.user.id, status: 'cart'}})
+    console.log('this is the order: ', order)
+    req.cart = order
+    req.session.cart = order
+    next()
   }
-
-  // below we check if there's already a cart associated with the userId
-  // if not, we create a new instance of the cart with the userId if logged in, and null if not
-  const currentCart = await Order.findOne({ where: { userId: req.user.id, status: 'cart' } }).catch(next);
-  if (currentCart) req.cart = currentCart;
-  else req.cart = await Order.create({ userId: req.user.id || null }).catch(next);
-
-  req.session.cartId = req.cart.id;
-  next();
-});
+  //not logged in
+  else {
+    const order = await Order.create({ where: { userId: 5, status: 'cart' }})
+    req.cart = order
+  }
+})
 
 router.get('/', (req, res, next) => {
-  res.send(req.cart);
-});
+  res.send(req.cart)
+})
 
 // add to cart
-router.post('/add-to-cart/products/:productId', async (req, res, next) => {
+router.put('/add-to-cart/products/:productId', async (req, res, next) => {
   const quantityToAdd = +req.body.quantityToAdd;
   const newProduct = await Product.findById(req.params.productId).catch(next);
   const [lineItem, wasCreated] = await LineItem.findOrCreate({ 
     where: { productId: req.params.productId, orderId: req.cart.id },  
     defaults: { quantity: req.body.quantityToAdd, price: newProduct.price }
   })
-  .catch(next);
+  .catch(next)
 
   if (!wasCreated) lineItem.quantity += quantityToAdd;
   await lineItem.save();
